6a7f9277f4377b835d3b24336cf4a139acf43e2c
Justin Rogers <justrog@gmail.com>
2014-12-06 12:26:47 -0800

    Get food and ore up and running in the resource manager.

--- TapCity/js/ResourceManager.js
+++ TapCity/js/ResourceManager.js
@@ -1,13 +1,65 @@
 ﻿"use strict";
 
-function ResourceManager() {
+(function () {
+    var supportedResources = [
+        "ore", "food", "units"
+    ];
+
     Object.defineProperties(this, {
+        ResourceManager: {
+            value: function ResourceManager() {
+                Object.defineProperties(this, {
+                    resources: {
+                        value: {},
+                        configurable: false,
+                        writable: false
+                    }
+                });
 
+                supportedResources.forEach(function (resource) {
+                    this.resources[resource] = 0;
+                }, this);
+                Object.seal(this.resources);
+            }
+        }
     });
-}
 
-ResourceManager.prototype = Object.create(Object.prototype);
+    ResourceManager.prototype = Object.create(Object.prototype);
 
-Object.defineProperties(ResourceManager.prototype, {
-
-});
+    Object.defineProperties(ResourceManager.prototype, {
+        addResource: {
+            value: function addResource(resourceType, resourceValue) {
+                if (resourceValue < 0 && Math.abs(resourceValue) > this.resources[resourceType]) {
+                    throw new Error("Negative resource value is out of range of available resources.")
+                }
+                this.resources[resourceType] += resourceValue;
+            }
+        },
+        addFood: {
+            value: function addFood(resourceValue) {
+                if (resourceValue < 0 && Math.abs(resourceValue) > this.resources["food"]) {
+                    throw new Error("Negative resource value is out of range of available resources.")
+                }
+                this.resources["food"] += resourceValue;
+            }
+        },
+        addOre: {
+            value: function addOre(resourceValue) {
+                if (resourceValue < 0 && Math.abs(resourceValue) > this.resources["ore"]) {
+                    throw new Error("Negative resource value is out of range of available resources.")
+                }
+                this.resources["ore"] += resourceValue;
+            }
+        },
+        ore: {
+            get: function get_ore() {
+                return this.resources["ore"];
+            }
+        },
+        food: {
+            get: function get_food() {
+                return this.resources["food"];
+            }
+        }
+    });
+}).call(window);
