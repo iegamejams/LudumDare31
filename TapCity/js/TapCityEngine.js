da55155b976071711e41da2c34eb89312e858472
Justin Rogers <justrog@gmail.com>
2014-12-06 13:23:25 -0800

    Hook up the ore clicks

--- TapCity/js/TapCityEngine.js
+++ TapCity/js/TapCityEngine.js
@@ -11,6 +11,26 @@ function TapCityEngine() {
             value: new Date(),
             configurable: false,
             writable: false
+        },
+        'ticksPerSecond': {
+            value: 30,
+            configurable: false,
+            writable: false
+        },
+        'orePerSecond': {
+            value: 0,
+            configurable: false,
+            writable: true
+        },
+        'foodPerSecond': {
+            value: 0,
+            configurable: false,
+            writable: true
+        },
+        'foodConsumedPerSecond': {
+            value: 0,
+            configurable: false,
+            writable: true
         }
     });
 }
@@ -32,6 +52,20 @@ Object.defineProperties(TapCityEngine.prototype, {
     clickFood: {
         value: function clickFood() {
             this.resources.addFood(1);
+        }
+    },
+    tick: {
+        value: function tick() {
+            /* TODO: If units/upgrades have changed recompute orePerSecond/foodPerSecond */
+            this.resources.addOre(this.orePerSecond / this.ticksPerSecond);
+            this.resources.addFood(this.foodPerSecond / this.ticksPerSecond);
+
+            var foodConsumed = -(this.foodConsumedPerSecond / this.ticksPerSecond);
+            if (foodConsumed > this.resources.food) {
+                foodConsumed = this.resources.food;
+                /* TODO: Warriors are rioting due to lack of food, halve effectiveness */
+            }
+            this.resources.addFood(foodConsumed);
         }
     }
 })
